6558aa91585b6c2f731d3c4e6a033ab30e561f15
augustin <augustin@ratatouille.(none)>
2013-06-06 19:21:22 +0200

    Only content is left

--- app/static/js/scripts.js
+++ app/static/js/scripts.js
@@ -1,24 +1,139 @@
-$.backstretch([
-  "http://farm9.staticflickr.com/8306/7856635672_cefdd93bd1_b.jpg"
-  , "http://farm3.staticflickr.com/2541/4129875543_939a4ba996_o.jpg"
-  , "http://farm4.staticflickr.com/3053/3007265056_e4b966bbeb_o.jpg"
-  ], {duration: 5000, fade: 750});
-
-
 jQuery(document).ready(function ($) {
+  var photos;
+
+  var probaPhotos = function() {
+    var probas = [];
+    var probasTotal = 0;
+    $.each(photos, function(i, photo) {
+      probas.push(photo.love);
+      probasTotal += photo.love;
+    });
+
+    return function() {
+      var rand = Math.random()*probasTotal;
+      for (var i=0; i<probas.length; i++) {
+        rand -= probas[i];
+        if (rand < 0) {
+          return i;
+        }
+      }
+      return probas.length -1;
+    }
+  }
+
+  $.getJSON('/photos', function(data) {
+    photos = data;
+
+    var getPhoto = probaPhotos();
+
+    function changePhoto() {
+      setTimeout(changePhoto, 8000);
+      var photo = photos[getPhoto()];
+      $('#count').html(photo.love);
+      $('#love').attr('data-url', photo.url);
+      $('#flag').attr('data-url', photo.url);
+      if (!photo.loved) {
+        $('#love').removeClass('done');
+      } else {
+        $('#love').addClass('done');
+      }
+      if (!photo.flagged) {
+        $('#flag').removeClass('done');
+      } else {
+        $('#flag').addClass('done');
+      }
+      return $.backstretch(photo.url, {fade: 2000});
+    }
+    changePhoto();
+  });
+
+  $('form').on('submit', function(e) {
+    var notice = $(this).find('.notice');
+
+    var regex = new RegExp('(http|ftp|https)://[a-z0-9\-_]+(\.[a-z0-9\-_]+)+([a-z0-9\-\.,@\?^=%&;:/~\+#]*[a-z0-9\-@\?^=%&;/~\+#])?', 'i');
+    if(!regex.test($(this).find('input[type="text"]').val())){
+      notice.html('Ceci n\'est pas une URL. Doit être au format http:\/\/site.com\/[...]');
+    } else {
+      notice.html('Envoi en cours');
+      $.ajax({
+        type: 'POST',
+        url: '/photos',
+        data: $(this).serialize(),
+        success: function(data) {
+          notice.html('Lien envoyé avec succès');
+        },
+        error: function(err) {
+          notice.html('Une erreur est survenue. Merci de réessayer');
+        }
+      });
+
+    }
+    e.preventDefault();
+    return false;
+  });
+
+  $('#love').click(function(e) {
+    var that = $(this)
+
+    for (var i=0; i<photos.length; i++) {
+      if (photos[i].url == that.attr('data-url')) {
+        photo = photos[i];
+        break;
+      }
+    }
+
+    if (!photo.loved) {
+
+      that.addClass('current');
+      $.ajax({
+        type: 'POST',
+        url: '/love',
+        data: 'url='+that.attr('data-url'),
+        success: function(data) {
+          that.removeClass('current');
+          that.addClass('done');
+          that.prev().html(parseInt(that.prev().html())+1);
+          photo.loved = true;
+        },
+        error: function(err) {
+          console.log('Une erreur est survenue.');
+        }
+      });
+    }
+  });
+
+  $('#flag').click(function(e) {
+    var that = $(this)
+
+    for (var i=0; i<photos.length; i++) {
+      if (photos[i].url == that.attr('data-url')) {
+        photo = photos[i];
+        break;
+      }
+    }
+
+    if (!photo.flagged) {
+      that.addClass('current');
+      $.ajax({
+        type: 'POST',
+        url: '/flag',
+        data: 'url='+that.attr('data-url'),
+        success: function(data) {
+          that.removeClass('current');
+          that.addClass('done');
+          photo.flagged = true;
+        },
+        error: function(err) {
+          console.log('Une erreur est survenue.');
+        }
+      });
+    }
+  });
 
   $('#title').animate({
     'margin-top': $(window).height()*0.5 - $('#title').height()/2,
     'margin-bottom': $(window).height()*0.5
   });
-
-  $(window).stellar();
-
-  var links = $('.navigation').find('li');
-  slide = $('.slide');
-  button = $('.button');
-  mywindow = $(window);
-  htmlbody = $('html,body');
 
   $('#content-beginning').waypoint(function(event, direction) {
     if (direction === 'down') {
@@ -32,9 +147,9 @@ jQuery(document).ready(function ($) {
     }
   });
 
-  slide.waypoint(function (event, direction) {
-
+  $('.slide').waypoint(function (event, direction) {
     dataslide = $(this).attr('data-slide');
+    console.log(dataslide, ' triggered');
 
     if (direction === 'down') {
       $('.navigation li[data-slide="' + dataslide + '"]').addClass('active').prev().removeClass('active');
@@ -43,36 +158,19 @@ jQuery(document).ready(function ($) {
       $('.navigation li[data-slide="' + dataslide + '"]').addClass('active').next().removeClass('active');
     }
 
-  });
-
-  // mywindow.scroll(function () {
-  //   if (mywindow.scrollTop() == 0) {
-  //     $('.navigation li[data-slide="1"]').addClass('active');
-  //     $('.navigation li[data-slide="2"]').removeClass('active');
-  //   }
-  // });
+  }, { offset: 80 });
 
   function goToByScroll(dataslide) {
-    console.log($('.slide[data-slide="' + dataslide + '"]').offset().top);
-    htmlbody.animate({
+    $('html,body').animate({
       scrollTop: $('.slide[data-slide="' + dataslide + '"]').offset().top-60
     }, 2000, 'easeInOutQuint');
   }
 
-
-
-  links.click(function (e) {
-    e.preventDefault();
-    dataslide = $(this).attr('data-slide');
-    goToByScroll(dataslide);
-  });
-
-  button.click(function (e) {
+  $('.innerLink').click(function (e) {
     e.preventDefault();
     dataslide = $(this).attr('data-slide');
     goToByScroll(dataslide);
 
   });
-
 
 });
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 var nodemailer = require("nodemailer");
+var fs = require('fs');
+
+var photosFile = __dirname + '/app/data/photos.json';
 
 // app.use(express.logger());
 app.use(express.errorHandler({
@@ -8,13 +11,85 @@ app.use(express.errorHandler({
 }));
 app.set('views', __dirname + '/app/views');
 app.set('view engine', 'jade');
+app.use(express.bodyParser());
 app.use(express['static'](__dirname + '/app/static'));
 
 app.get('/', function(req, res) {
-  res.render('index', {});
+  res.render('index');
+});
+
+app.get('/photos', function(req, res) {
+  fs.readFile(photosFile, 'utf8', function (err, data) {
+    res.setHeader('Content-Type','application/json');
+    res.end(data);
+  });
+});
+
+app.post('/photos', function(req, res) {
+  openFileForModif(function(photos) {
+    photos.push({
+      url: req.body.url,
+      love: 1,
+      flag: 0
+    });
+    return photos;
+  });
+});
+
+app.post('/love', function(req, res) {
+  openFileForModif(req, res, function(photos) {
+    for (var i=0; i< photos.length; i++) {
+      if(photos[i].url == req.body.url) {
+        photo = photos[i];
+        photo.love +=1;
+        photos[i] = photo;
+        break;
+      }
+    }
+    return photos;
+  });
+});
+
+app.post('/flag', function(req, res) {
+  openFileForModif(req, res, function(photos) {
+    for (var i=0; i< photos.length; i++) {
+      if(photos[i].url == req.body.url) {
+        photo = photos[i];
+        photo.flag +=1;
+        photos[i] = photo;
+        break;
+      }
+    }
+
+    if(photos[i].flag > 10) {
+      photos.splice(i, 1);
+    }
+
+    return photos;
+  });
 });
 
 var port = process.env.PORT || 8000;
 app.listen(port, function() {
   console.log('Listening on port ' + port + ' in ' + app.get('env') + ' mode');
 });
+
+var openFileForModif = function(req, res, modif) {
+  fs.readFile(photosFile, 'utf8', function (err, data) {
+    if (err) {
+      return console.log('Error: ' + err);
+    }
+    var photos = JSON.parse(data);
+
+    photos = modif(photos);
+
+    fs.writeFile(photosFile, JSON.stringify(photos), function(err) {
+      if (err) {
+        return console.log('Error: ' + err);
+      }
+      res.setHeader('Content-Type','application/json');
+      res.end('{"status": 200, "text": "Successfull"}');
+    });
+
+  });
+};
